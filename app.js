0a986d17b6f13132374f061355904e627066161c
Joao <ritterjv@gmail.com>
2019-08-31 11:41:30 -0700

    -

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+const express = require("express");
+const app = express();
+
 const add = require("@sustainer-network/event-store-add-service");
 const tokensFromReq = require("@sustainer-network/tokens-from-req");
+const middleware = require("@sustainer-network/event-store-middleware");
 
-exports.add = (req, res) => {
+middleware(app);
+
+app.post("/", (req, res) => {
   add({ params: req.body, tokens: tokensFromReq(req) })
     .then(() => res.send({}))
     .catch(e => res.status(e.statusCode || 500).send(e));
-};
+});
