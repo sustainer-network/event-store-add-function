330299ed3ed1edd98296bcd753b3b1c8c860bb5c
Joao <ritterjv@gmail.com>
2019-09-03 17:34:23 -0700

    -

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const app = express();
 const add = require("@sustainers/event-store-add-service");
 const tokensFromReq = require("@sustainers/tokens-from-req");
 const middleware = require("@sustainers/event-store-middleware");
+const logger = require("@sustainers/logger");
 
 middleware(app);
 
 app.post("/", (req, res) => {
   add({ params: req.body, tokens: tokensFromReq(req) })
     .then(() => res.send({}))
-    .catch(e => res.status(e.statusCode || 500).send(e));
+    .catch(e => {
+      logger.error("behehehed", { e, stack: e.stack });
+      res.status(e.statusCode || 500).send(e);
+    });
 });
 
 module.exports = app;
